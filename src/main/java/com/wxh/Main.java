899acbd717a73058f321e2215fcb18f58a5a7ad2
Wangxh1016 <3122979073@qq.com1>
2023-06-06 20:15:41 +0800

    重新测试冲突

--- src/main/java/com/wxh/Main.java
+++ src/main/java/com/wxh/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("master");
-        System.out.println("master3");
         System.out.println("master master");
         System.out.println("master master master");
     }
