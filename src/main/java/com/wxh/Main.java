22280876a6ee825a07fbaf8fec9314738140ef1e
Wangxh1016 <3122979073@qq.com1>
2023-06-06 20:06:25 +0800

    idea test4 git hot-fix分支

--- src/main/java/com/wxh/Main.java
+++ src/main/java/com/wxh/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("master");
+        System.out.println("master master");
+        System.out.println("hot-fix hot-fix");
     }
 }
