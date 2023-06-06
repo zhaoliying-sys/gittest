a2f5eba2f2fcb637c9618ec1889b0b68d04d7e78
Wangxh1016 <3122979073@qq.com1>
2023-06-06 20:16:30 +0800

    重新测试冲突2

--- src/main/java/com/wxh/Main.java
+++ src/main/java/com/wxh/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("master");
+        System.out.println("master3");
         System.out.println("master master");
         System.out.println("master master master");
     }
